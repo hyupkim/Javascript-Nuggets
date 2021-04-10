6f22df0c364020b49261cf80ed33918a9e280a7f
hyupkim <makeyeezy@gmail.com>
2021-04-10 11:00:35 -0400

    Filter and Find

---
+++ FilterAndFind.js
@@ -0,0 +1,44 @@
+// Filter - returns a new array, can manipulate the size of new array (unlike map),
+// returns based on the condition
+// Find - returns single instance, returns the first match, if no match - undefined
+
+const people = [
+  { name: "bob", age: 20, position: "developer" },
+  { name: "peter", age: 25, position: "designer" },
+  { name: "susy", age: 30, position: "the boss" },
+  { name: "anna", age: 35, position: "intern" },
+];
+const fruits = ["orange", "pear", "lemon"];
+
+// filter
+const youngPeople = people.filter((person) => {
+  // if (person.age < 30) {
+  //     return person;
+  // }
+  return person.age < 30;
+});
+console.log(youngPeople);
+const developers = people.filter((person) => person.position === "developer");
+console.log(developers);
+
+// no match
+const seniorDevs = people.filter((item) => item.position === "senior dev");
+console.log(seniorDevs);
+
+// find
+const peter = people.find((person) => person.name === "peter");
+console.log(peter);
+const fruit = fruits.find((fruit) => fruit === "lemon");
+console.log(fruit);
+
+// no match
+const oldPerson = people.find((person) => person.age > 35);
+console.log(oldPerson);
+
+// multiple matches - first match
+const randomPerson = people.find((person) => person.age < 35);
+console.log(randomPerson);
+
+const anna = people.filter((person) => person.name === "anna");
+console.log(peter.position);
+console.log(anna[0].position);
